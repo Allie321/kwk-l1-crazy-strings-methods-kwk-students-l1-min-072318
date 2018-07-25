a4de0d67aeabe9894acf4172e27f53cbf7fea614
Allie321 <allisonroberson04@gmail.com>
2018-07-25 20:56:40 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -3,4 +3,9 @@ crazy_strings
 def crazy_strings(a,b)
 "hello friends".upcase.reverse
 end
-puts crazy_strings(a,b)
+puts crazy_strings
+
+def crazy_strings(a,b)
+  "hello friends".gsub("s","z")
+end
+puts crazy_strings
